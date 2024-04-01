e2bb8bf45e77f754b5a2284aac761272f0824b8c
Labomi <labomiaiyere@gmail.com>
2024-04-01 16:46:33 +0100

    added git clone description

---
+++ git_basics.py
@@ -0,0 +1,12 @@
+git_commands = {
+    "git clone": "Makes a local copy of code in a remote repository, onto your computer.",
+    "git add": "",
+    "git commit": "",
+    "git status": "",
+    "git push": "",
+    "git pull": "",
+    "git branch": "",
+    "git merge": "",
+}
+
+print(git_commands)
